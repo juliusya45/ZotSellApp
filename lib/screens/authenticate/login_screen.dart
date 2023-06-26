86e47d86e8f489ed758ead625c0388ebd47bee64
Julius Yang <68701101+juliusya45@users.noreply.github.com>
2023-06-26 15:07:44 -0700

    login screen ui

--- lib/screens/authenticate/login_screen.dart
+++ lib/screens/authenticate/login_screen.dart
@@ -8,22 +8,140 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //from https://stackoverflow.com/questions/67768950/how-to-add-specific-fixed-value-to-textfield
+  final TextEditingController _textController = TextEditingController();
+  final String _userPostfix = '@uci.edu';
+
+  //followed this tutorial: https://www.youtube.com/watch?v=aJdIkRipgSk&list=PLlvRDpXh1Se4wZWOWs8yapI8AS_fwDHzf&index=3
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 190, 236, 192),
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: Column(children: [
-          //Header text to welcome user
-          Text('Welcome!'),
-          //username/email textfield
-      
-          //password textfield
-      
-          //sign in button
-      
-          //new user? register now
-        ]),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            //logo would go here
+            Icon(Icons.shopping_basket_outlined, size: 100),
+            SizedBox(height: 75),
+            //Header text to welcome user
+            Text(
+              'Welcome!',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 36,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Please login or register below',
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(height: 50),
+
+            //username/email textfield
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: TextField(
+                    //forces user to put in @uci.edu email
+                    //that way we can have only students register
+                    controller: _textController,
+                    onChanged: (value) {
+                        if (value == _userPostfix) {
+                          _textController.text = "";
+                          return;
+                        }
+                        value.endsWith(_userPostfix)
+                            ? _textController.text = value
+                            : _textController.text = value + _userPostfix;
+                        _textController.selection = TextSelection.fromPosition(
+                            TextPosition(
+                                offset: _textController.text.length -
+                                    _userPostfix.length));
+                      },
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Email@uci.edu',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            //password textfield
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Password',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            //sign in button
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                padding: EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                  color: Colors.green[300],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(child: Text(
+                  'Sign In',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                  )),
+              ),
+            ),
+            SizedBox(height: 25),
+
+            //new user? register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Don\'t have an account?',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  )
+                ),
+                Text(
+                  ' Register Now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    )
+                  )
+              ],
+            ),
+          ]),
+        ),
       )
     );
   }
